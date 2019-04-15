bae15f08b07a7eaff1be99c8bae57c08acc9ed5d
jhonatadev <xxxonata@gmail.com>
2019-04-15 13:06:04 -0300

    NavBar && App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './NavBar';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      github: {
+        url: 'https://api.github.com/users',
+        client_id: 'e53febbd554644d70b00',
+        client_secret: '0eabb5d3af6d3ae419c42913f660f0523a19140e',
+        count: 7,
+        sort: 'created: asc'
+      },
+      user: [],
+      repos: []
+    }
+  }
+
+  getUser = (e) => {
+    const user = e.targe.value;
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <NavBar/>
+
+        <div className="container">
+          <div className="card card-body">
+            <h1>Pesquisar Usuarios do GitHub</h1>
+            <p className="lead">Digite um nome para encontrar usuários e repositórios</p>
+            <input onChange={this.getUser} id="search" type="text" className="form-control" required/>
+          </div>        
+        </div>
       </div>
     );
   }
---
+++ src/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => (
+	<nav className="navbar navbar-dark bg-info mb-3">
+		<div className="container">
+			<a href="#" className="navbar-brand text-white">Github Finder</a>
+		</div>
+	</nav>
+)
+
+export default NavBar;
