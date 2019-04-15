9b52806e2e8a3bccf9ade2ce57c2316490a6427a
jhonatadev <xxxonata@gmail.com>
2019-04-15 13:37:20 -0300

    Recebando dados da API, Profile.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import NavBar from './NavBar';
+import Profile from './Profile';
 
 class App extends Component {
   constructor(){
@@ -18,9 +20,18 @@ class App extends Component {
   }
 
   getUser = (e) => {
-    const user = e.targe.value;
-  }
+    const user = e.target.value;
+
+    const { url, client_id, client_secret, count, sort } = this.state.github;
+    
+      axios.get(
+        `${url}/${user}?client_id=${client_id}&client_secret=${client_secret}`
+      )
+      .then(({ data }) => this.setState({ user: data}));
+    }
+  
   render() {
+    const { user } = this.state;
     return (
       <div className="App">
         <NavBar/>
@@ -32,6 +43,8 @@ class App extends Component {
             <input onChange={this.getUser} id="search" type="text" className="form-control" required/>
           </div>        
         </div>
+
+        { user.length !== 0 ? <Profile user={user}/> : null}
       </div>
     );
   }
